1220e74cda6eccedea3abf1aba1eb62eabd39caf
divine <divineobi07@gmail.com>
2022-07-09 17:25:31 +0100

    Modal component

---
+++ src/components/Header/Modal.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import "./Modal.css";
+
+const Modal = () => {
+  return (
+    <nav className="modal">
+      <div className="modal_1">
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">Resources</a>
+      </div>
+
+      <div className="modal_2">
+        <button className="login_btn">Login</button>
+        <button className="sign_btn">Sign Up</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Modal;
