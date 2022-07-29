64366350ba8c82520eb666bb59105bd5da20c4e3
divine <divineobi07@gmail.com>
2022-07-29 23:33:17 +0100

    add Loader component

---
+++ src/components/Header/Loader.js
@@ -0,0 +1,36 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <span className="loader_gif">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50px"
+        height="50px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="#0a0a0a"
+          strokeWidth="10"
+          r="35"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+          ></animateTransform>
+        </circle>
+      </svg>
+    </span>
+  );
+};
+
+export default Loader;
